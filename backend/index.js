a9fa675367bf6494ae0c60f76435c73bdad3c965
meg1770 <meghnabal70@gmail.com>
2025-07-14 21:58:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -165,6 +165,6 @@ app.get('/api/stocks', async (_req, res) => {
 });
 
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(` Server running at http://localhost:${PORT}`);
 });
