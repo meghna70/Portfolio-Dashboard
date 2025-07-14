0c458c941d1364fa6a01a5a50430e4b6d883b0ad
meg1770 <meghnabal70@gmail.com>
2025-07-14 12:55:58 +0530

    Update page.tsx

--- frontend/src/app/portfolio/page.tsx
+++ frontend/src/app/portfolio/page.tsx
@@ -77,6 +77,8 @@ export default function PortfolioPage() {
         let isMounted = true;
         const fetchData = async () => {
             try {
+                console.log(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/stocks \n`)
+                 console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
                 const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/stocks`); // fetch is called in api/stocks/route
                 const json = await res.json();
                 const modified = json.map((stock: Stock) => ({
